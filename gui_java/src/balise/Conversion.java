3fc2623936e50650ec93fe137ab57acdeda78033
PF <pierre-francois.gimenez@laposte.net>
2016-07-25 16:05:59 +0200

    Conversion entre deux formats de fichiers

---
+++ gui_java/src/balise/Conversion.java
@@ -0,0 +1,90 @@
+package balise;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Conversion de fichier
+ * @author pf
+ *
+ */
+
+public class Conversion {
+
+	public static void main(String[] args)
+	{
+/*		args = new String[2];
+		args[0] = "../Benchmark/P1.txt";
+		args[1] = "../Benchmark/out.txt";
+	*/	
+		if(args.length != 2)
+		{
+			System.out.println("Utilisation : java -jar conversion.jar input-file output-file");
+			return;
+		}
+		
+		BufferedReader br;
+		BufferedWriter out;
+		
+		try {
+			br = new BufferedReader(new FileReader(args[0]));
+		} catch (FileNotFoundException e1) {
+			System.err.println(e1);
+			return;
+		}
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "utf-8"));
+		} catch (UnsupportedEncodingException e1) {
+			System.err.println(e1);
+			try {
+				br.close();
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+			return;
+		} catch (FileNotFoundException e1) {
+			try {
+				br.close();
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+			System.err.println(e1);
+			return;
+		}
+			
+		try {
+			out.write("# T3 patented");
+			out.newLine();
+			while(true)
+			{
+				String[] timestamps = new String[3];
+				for(int i = 0; i < 3; i++)
+				{
+					String line = br.readLine();
+					if(line == null)
+					{
+						out.close();
+					    br.close();		
+					    return;	
+					}			
+					System.out.println(line);
+					timestamps[i] = line.split(";")[1];
+				}
+				out.write(timestamps[0]+";"+timestamps[1]+";"+timestamps[2]);
+				out.newLine();
+			}
+			
+
+		} catch (IOException e) {
+			return;
+		}	
+
+	}
+
+}
