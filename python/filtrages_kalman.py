aa5502f274303c9c954c2e7f07956766533c597e
spraakforskaren <clemsciences@aol.com>
2016-07-26 23:14:10 +0200

    Des entiers pour les positions !!!

--- python/filtrages_kalman.py
+++ python/filtrages_kalman.py
@@ -1184,7 +1184,7 @@ def script_trajectoire_filtree_test_p1():
     :return:
     """
     print "script_classic_trajectory_with_real_measures"
-    dt = 0.025
+    dt = 0.012
     measures_pos = np.genfromtxt("/home/clemsciences/PycharmProjects/balise-t3/Benchmark/P1-brut.txt", delimiter="\t")
     real_path = []
     for i in range(measures_pos.shape[0]):
@@ -1197,13 +1197,13 @@ def script_trajectoire_filtree_test_p1():
     measures = np.concatenate((measures_pos, vite), axis=1)
     measures = np.asmatrix(measures)
     filtering = FiltrageKalman(measures[0, :].T, dt=dt)
-    with open("./filtered_values_pos_to_pos/P1-brut-filtre-25ms.txt", "w") as f:
+    with open("./filtered_values_pos_to_pos/P1-brut-filtre-12ms.txt", "w") as f:
         for i in range(1, measures.shape[0]):
             x = measures[i, 0]
             y = measures[i, 1]
             filtering.update(x, y)
             pos = filtering.get_current_position()
-            f.write(str(pos.x)+"\t"+str(pos.y)+"\n")
+            f.write(str(int(pos.x))+"\t"+str(int(pos.y))+"\n")
             l_pos_filtre.append(pos)
     print get_mer(real_path, l_pos_filtre)
 
